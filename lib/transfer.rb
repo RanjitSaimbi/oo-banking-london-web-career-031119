a48893d757fe9421b9af3968c111cf3d861c9dd1
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-14 11:34:47 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount 
+  
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end 
+  
+  def valid?
+    if self.receiver.class == BankAccount && self.sender.class == BankAccount
+      true 
+    else
+      false 
+    end 
+  end 
+  
 end
+
