c3eb35e1f4fc09fc5b34c2dd1868b1d5a7a975f0
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-14 13:08:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,11 +20,11 @@ class Transfer
   end 
   
   def execute_transaction
-    if 
+    if @status == "pending"
       @sender.balance -= amount
       @receiver.balance += amount
       @status = "complete"
-    
+    end 
   end 
   
 end
